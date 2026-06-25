27650025fae3a4e2ea88c8f583fff89973c74e4e
taroho <136779268+taroho@users.noreply.github.com>
2026-06-25 23:29:14 +0900

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,14 +20,26 @@ require("colors.mytheme").setup()
 -- ほかの設定もここに追記していく
 vim.o.number = true  -- 行番号表示
 vim.o.expandtab = true  -- タブはスペースに変換
-vim.o.shiftwidth = 4    -- インデント幅2スペース
-vim.o.tabstop = 4       -- タブ幅2スペース
+vim.o.shiftwidth = 2    -- インデント幅2スペース
+vim.o.tabstop = 2       -- タブ幅2スペース
 vim.o.smartindent = true -- インデント自動調整
 
 vim.o.hlsearch = true   -- 検索結果ハイライト
 vim.o.incsearch = true  -- インクリメンタル検索
 
 vim.o.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = [[powershell.exe -NoProfile -Command "(Get-Clipboard) -replace \"`r\", \"\""]],
+    ["*"] = [[powershell.exe -NoProfile -Command "(Get-Clipboard) -replace \"`r\", \"\""]],
+  },
+  cache_enabled = 0,
+}
 vim.o.termguicolors = true  -- 24bitカラーを有効に
 -- カラースキームは好きなものをセット
 vim.o.laststatus = 2
@@ -40,53 +52,67 @@ vim.o.guicursor = table.concat({
     "n-v-c-r-cr:block-Cursor",       -- normal, visual, command , rはブロック
     "i-ci-ve:ver30-CursorI",   -- insert は縦棒
 }, ",")
+vim.o.list = true
+vim.opt.listchars = {
+  space = "·",
+  tab = "→ ",
+  trail = "·",
+  extends = "»",
+  precedes = "«",
+  nbsp = "␣",
+}
+vim.opt.listchars:append("lead:·")
+
 
 
 require("lazy").setup({
-{
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  build = ":TSUpdate",
---   config = function()
---     require("nvim-treesitter.configs").setup({
---       ensure_installed = { "go", "lua", "javascript", "html", "css", "php" }, -- インストールする言語
---       highlight = {
---         enable = true,  -- ハイライトを有効化
---         additional_vim_regex_highlighting = false, -- Vimの正規表現ハイライトは無効化
---       },
---     })
---   end,
-},
+  -- nvim-treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install({
+        "lua", "javascript", "typescript", "tsx", "python", "java",
+      })
 
+      -- 対象ファイルタイプでハイライトを有効化
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "lua", "javascript", "typescript", "typescriptreact", "python", "java" },
+        callback = function()
+          vim.treesitter.start()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+  -- indent-blankline
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      vim.cmd([[highlight IndentRed guifg=#e06c75]])
+      vim.cmd([[highlight IndentYellow guifg=#e5c07b]])
+      vim.cmd([[highlight IndentBlue guifg=#61afef]])
+      vim.cmd([[highlight IndentGreen guifg=#98c379]])
+      vim.cmd([[highlight IndentViolet guifg=#c678dd]])
+      vim.cmd([[highlight IndentCyan guifg=#56b6c2]])
 
---   {
---     "nvim-neo-tree/neo-tree.nvim",
---     branch = "v3.x",
---     dependencies = {
---       "nvim-lua/plenary.nvim",
---       "nvim-tree/nvim-web-devicons",
---       "MunifTanjim/nui.nvim",
---     },
---     config = function()
---       require("neo-tree").setup({
---         follow_current_file = {
---           enabled = true,
---           leave_dirs_open = false,
---         },
---         window = {
---           position = "left",
---           width = 30,
---         },
---       })
---     end,
---   },
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
-  callback = function(ctx)
-    -- 必要に応じて`ctx.match`に入っているファイルタイプの値に応じて挙動を制御
-    -- `pcall`でエラーを無視することでパーサーやクエリがあるか気にしなくてすむ
-    pcall(vim.treesitter.start)
-  end,
+      require("ibl").setup({
+        indent = {
+          highlight = {
+            "IndentRed",
+            "IndentYellow",
+            "IndentBlue",
+            "IndentGreen",
+            "IndentViolet",
+            "IndentCyan",
+          },
+        },
+        scope = {
+          enabled = true,
+        },
+      })
+    end
+  },
 })
